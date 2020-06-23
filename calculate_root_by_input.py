1c48fcf882dca1764eeb43551fbe477b1ef45f85
vviilloovv <vviilloovv@gmail.com>
2020-06-23 14:29:14 +0900

    関数化

--- calculate_root_by_input.py
+++ calculate_root_by_input.py
@@ -1,26 +1,35 @@
 import math
 
-while True:
-    x = input("Input positive integer: 'end' is end: ")
+def get_positive_numeral():
+    "自然数を標準入力"
 
-    if(x == "end"):
-        print("bye.")
-        break
+    while True:
+        x = input("Input positive integer: 'end' is end: ")
 
-    try:
-        x = float(x)
-    except ValueError:
-        print(x, "is not number!")
-        continue
-    except:
-        print("bad error.")
-        continue
+        if(x == "end"):
+            print("bye.")
+            return x
 
-    if(x <= 0):
-        print(x, "is not positive!")
-        continue
+        try:
+            x = float(x)
+        except ValueError:
+            print(x, "is not number!")
+            continue
+        except:
+            print("bad error.")
+            continue
+
+        if(x <= 0):
+            print(x, "is not positive!")
+            continue
+
+        return x
+
+def square_root(x):
+    "引数xの平方根を求める"
 
     rnew = x
+
     diff = abs(rnew - x / rnew)
     while(1.0E-6 < diff):
         r1 = rnew
@@ -28,4 +37,14 @@ while True:
         rnew = (r1 + r2) / 2
         print(r1, rnew, r2)
         diff = abs(r1 - r2)
+
+    return rnew
+
+while True:
+    x = get_positive_numeral()
+
+    if(x == "end"):
+        break
+
+    print(square_root(x))
     print("----------")

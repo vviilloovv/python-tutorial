d0d99a546b42618c562e540647a2a2b760695488
vviilloovv <vviilloovv@gmail.com>
2020-06-24 17:37:07 +0900

    ラムダ関数を使った書き換え、フォント指定

--- tkdemo_plus_calculate.py
+++ tkdemo_plus_calculate.py
@@ -21,36 +21,6 @@ def do_equal():
     result = first_term + second_term
     current_number = 0
 
-def key1():
-    key(1)
-
-def key2():
-    key(2)
-
-def key3():
-    key(3)
-
-def key4():
-    key(4)
-
-def key5():
-    key(5)
-
-def key6():
-    key(6)
-
-def key7():
-    key(7)
-
-def key8():
-    key(8)
-
-def key9():
-    key(9)
-
-def key0():
-    key(0)
-
 def key(n):
     global current_number
     current_number = current_number * 10 + n
@@ -80,19 +50,19 @@ f = tk.Frame(root)
 f.grid()
 
 # ウィジェット作成
-b1 = tk.Button(f,text='1', command = key1)
-b2 = tk.Button(f,text='2', command = key2)
-b3 = tk.Button(f,text='3', command = key3)
-b4 = tk.Button(f,text='4', command = key4)
-b5 = tk.Button(f,text='5', command = key5)
-b6 = tk.Button(f,text='6', command = key6)
-b7 = tk.Button(f,text='7', command = key7)
-b8 = tk.Button(f,text='8', command = key8)
-b9 = tk.Button(f,text='9', command = key9)
-b0 = tk.Button(f,text='0', command = key0)
-bc = tk.Button(f,text='C', command = clear)
-bp = tk.Button(f,text='+', command = plus)
-be = tk.Button(f,text="=", command = equal)
+b1 = tk.Button(f,text='1', command = lambda: key(1), font=("Helvetica", 14))
+b2 = tk.Button(f,text='2', command = lambda: key(2), font=("Helvetica", 14))
+b3 = tk.Button(f,text='3', command = lambda: key(3), font=("Helvetica", 14))
+b4 = tk.Button(f,text='4', command = lambda: key(4), font=("Helvetica", 14))
+b5 = tk.Button(f,text='5', command = lambda: key(5), font=("Helvetica", 14))
+b6 = tk.Button(f,text='6', command = lambda: key(6), font=("Helvetica", 14))
+b7 = tk.Button(f,text='7', command = lambda: key(7), font=("Helvetica", 14))
+b8 = tk.Button(f,text='8', command = lambda: key(8), font=("Helvetica", 14))
+b9 = tk.Button(f,text='9', command = lambda: key(9), font=("Helvetica", 14))
+b0 = tk.Button(f,text='0', command = lambda: key(0), font=("Helvetica", 14))
+bc = tk.Button(f,text='C', command = clear, font=("Helvetica", 14))
+bp = tk.Button(f,text='+', command = plus, font=("Helvetica", 14))
+be = tk.Button(f,text="=", command = equal, font=("Helvetica", 14))
 
 # グリッドによるウィジェットの割付
 b1.grid(row=3,column=0)
